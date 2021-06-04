2d3c491946d90a2cc1e43322713cfca5055986bc
vantamit99 <vantamit99@gmail.com>
2021-06-04 15:13:58 +0700

    change port deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const cors = require('cors')
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
